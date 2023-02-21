5f32b21cd7d7ae6db66137673d3a5f1236b947db
Ruturaj B <ruturajhb2003@gmail.com>
2023-02-21 21:11:17 +0530

    Project: Project Planner; Add Full Functionality to Buttons And Implement Drag And Drop

--- Project Planner/assets/scripts/app.js
+++ Project Planner/assets/scripts/app.js
@@ -15,6 +15,9 @@ class DOMHelper {
         );
         // Append will move the element if it is already present in the dom and not copy it
         destinationElement.append(element);
+
+        //Scroll to or scroll by also support smooth
+        element.scrollIntoView({ behavior: 'smooth' });
     }
 }
 
@@ -30,6 +33,7 @@ class Component {
     detach = () => {
         if (this.element) {
             this.element.remove();
+            // this.element.parentElement.removeChild(this.element);
         }
     };
 
@@ -45,18 +49,28 @@ class Component {
 class Tooltip extends Component {
     constructor(closeNotifierFunction, extraInfo, hostElementId) {
         super(hostElementId);
-        this.closeNoti = closeNotifierFunction;
+        this.closeNotifier = closeNotifierFunction;
         this.text = extraInfo;
         this.render();
     }
     closeTooltip = () => {
         this.detach();
+        this.closeNotifier();
     };
 
     render() {
         const tooltipElement = document.createElement('div');
         tooltipElement.className = 'card';
-        tooltipElement.textContent = this.text;
+        //tooltipElement.textContent = this.text;
+
+        //This will give us acess to template that is in Html
+        const toolTipTemplate = document.getElementById('tooltip');
+        //It will give us content of template ie: h2 and p tag
+        const toolTipBody = document.importNode(toolTipTemplate.content, true);
+        toolTipBody.querySelector('p').textContent = this.text;
+
+        tooltipElement.append(toolTipBody);
+
         console.log(this.hostElement.getBoundingClientRect());
 
         //Pos from 0,0 to the left Most point of our Element
@@ -94,7 +108,8 @@ class ProjectItem {
         this.updateProjectListsHandler = updateProjectListsFunction;
 
         this.connectMoreInfoButton();
-        this.connectSwitchButton();
+        this.connectSwitchButton(type);
+        this.connectDrag();
     }
 
     showMoreInfoHandler() {
@@ -115,6 +130,15 @@ class ProjectItem {
         this.hasActiveToolTip = true;
     }
 
+    connectDrag() {
+        document
+            .getElementById(this.id)
+            .addEventListener('dragstart', (event) => {
+                event.dataTransfer.setData('text/plain', this.id);
+                event.dataTransfer.effectAllowed = 'move';
+            });
+    }
+
     connectMoreInfoButton() {
         const projectItemElement = document.getElementById(this.id);
         const moreInfoBtn = projectItemElement.querySelector('button');
@@ -130,11 +154,12 @@ class ProjectItem {
         // We use let since we are Deep Cloninng and replacing it
         let switchBtn = projectItemElement.querySelector('button:last-of-type');
         switchBtn = DOMHelper.clearEventListners(switchBtn);
-        switchBtn.textContent = type === 'active' ? 'Finish' : 'Activate';
+
         switchBtn.addEventListener(
             'click',
             this.updateProjectListsHandler.bind(null, this.id)
         );
+        switchBtn.textContent = type === 'finished' ? 'Activate' : 'Finish';
     }
 
     update(updateProjectListsFunction, type) {
@@ -162,8 +187,51 @@ class ProjectList {
             );
         }
         console.log(this.projects);
+        this.connectDroppable();
     }
+    connectDroppable() {
+        const list = document.querySelector(`#${this.type}-projects ul`);
 
+        list.addEventListener('dragenter', (event) => {
+            //We cant read the data just type
+            if (event.dataTransfer.types[0] === 'text/plain') {
+                event.preventDefault();
+                // Parent will be section
+                list.parentElement.classList.add('droppable');
+            }
+        });
+        list.addEventListener('dragover', (event) => {
+            if (event.dataTransfer.types[0] === 'text/plain') {
+                event.preventDefault();
+            }
+        });
+
+        list.addEventListener('dragleave', (event) => {
+            if (
+                event.relatedTarget.closest(`#${this.type}-projects ul`) !==
+                list
+            ) {
+                list.parentElement.classList.remove('droppable');
+            }
+        });
+
+        list.addEventListener('drop', (event) => {
+            // We can get any objects that we passed during DragStart
+            // This will retrive the Data
+            const prjId = event.dataTransfer.getData('text/plain');
+            // To avoid Duplication
+            if (this.projects.find((p) => p.id === prjId)) {
+                return;
+            }
+            // We can do complicated way like we did for the swap of active and finsh but here we do a big brain move
+            document
+                .getElementById(prjId)
+                .querySelector('button:last-of-type')
+                .click();
+            list.parentElement.classList.add('droppable');
+            event.preventDefault(); // Not Required0
+        });
+    }
     setSwitchHandlerFunction(switchHandlerFunction) {
         this.switchHandeler = switchHandlerFunction;
     }
